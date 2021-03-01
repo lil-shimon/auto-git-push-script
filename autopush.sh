5eef4299c8223db3f2c66788e27473b0bf493d1a
shimosawa <shimosawa@questar.ac>
2021-03-01 20:08:40 +0900

    initial commit

---
+++ autopush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Root dir --> project path
+DIR="${HOME}/project"
+# app file path
+newline_file="/logs.js"
+
+echo "">>$DIR$newline_file
+
+#commit and push
+cd $DIR
+git add .
+git commit -m"自動更新"
+git push origin master
